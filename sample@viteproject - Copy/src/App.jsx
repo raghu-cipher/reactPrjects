4fd42225337cf1045a7a5f846fd06e392de978ee
raghu-cipher <raghunow3@gmail.com>
2025-04-06 13:02:07 +0530

    first-commit

---
+++ sample@viteproject - Copy/src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+const App = () => {
+    return (
+        <>
+            <h1>Welcome React</h1>
+            <h5>hello world</h5>
+        </>
+    )
+}
+
+export default App
